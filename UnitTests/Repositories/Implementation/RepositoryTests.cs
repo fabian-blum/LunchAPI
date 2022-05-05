56ae443b6d24818f159bd8ac3e4848529d73788d
Fabian Blum <fa.blum@douglas.de>
2022-05-05 08:46:11 +0200

    Add tests

---
+++ UnitTests/Repositories/Implementation/RepositoryTests.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using LunchAPI.Models;
+using LunchAPI.Repositories.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace UnitTests.Repositories.Implementation
+{
+    public class RepositoryTests
+    {
+
+        public RepositoryTests()
+        {
+
+        }
+
+        private Repository<Meal> CreateRepository()
+        {
+            return new Repository<Meal>();
+        }
+
+        [Fact]
+        public void Add_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var repository = this.CreateRepository();
+            var enity = new Meal()
+            {
+                Id = Guid.NewGuid(),
+                Ingredients = new List<Ingredients>()
+                {
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid()
+                    }
+                }
+            };
+
+            // Act
+            repository.Add(enity);
+
+            // Assert
+            var entries = repository.GetAll();
+            entries.Should().HaveCount(1);
+            entries.FirstOrDefault().Should().NotBeNull();
+            entries.FirstOrDefault()?.Ingredients.Should().HaveCount(2);
+        }
+    }
+}
