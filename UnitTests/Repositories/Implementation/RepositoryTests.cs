fb5fdfd37725c801fd97ad92c7b6b43ce4152404
Fabian Blum <fa.blum@douglas.de>
2022-05-05 09:01:26 +0200

    Add update and delete test

--- UnitTests/Repositories/Implementation/RepositoryTests.cs
+++ UnitTests/Repositories/Implementation/RepositoryTests.cs
@@ -50,6 +50,82 @@ namespace UnitTests.Repositories.Implementation
             entries.Should().HaveCount(1);
             entries.FirstOrDefault().Should().NotBeNull();
             entries.FirstOrDefault()?.Ingredients.Should().HaveCount(2);
+
+            var one = repository.Get(enity.Id);
+            one.Should().NotBeNull();
+            one?.Id.Should().Be(enity.Id);
+        }
+
+        [Fact]
+        public void Update_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var repository = this.CreateRepository();
+            var enity = new Meal()
+            {
+                Id = Guid.NewGuid(),
+                Ingredients = new List<Ingredients>()
+                {
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid()
+                    }
+                }
+            };
+
+            // Act
+            repository.Add(enity);
+
+            enity.Ingredients = new List<Ingredients>()
+            {
+                new Ingredients()
+                {
+                    Id = Guid.NewGuid()
+                }
+            };
+
+            repository.Update(enity);
+
+            // Assert
+            var entries = repository.GetAll();
+            entries.Should().HaveCount(1);
+            entries.FirstOrDefault().Should().NotBeNull();
+            entries.FirstOrDefault()?.Ingredients.Should().HaveCount(1);
+        }
+
+        [Fact]
+        public void Delete_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var repository = this.CreateRepository();
+            var enity = new Meal()
+            {
+                Id = Guid.NewGuid(),
+                Ingredients = new List<Ingredients>()
+                {
+                    new()
+                    {
+                        Id = Guid.NewGuid(),
+                    },
+                    new()
+                    {
+                        Id = Guid.NewGuid()
+                    }
+                }
+            };
+
+            // Act
+            repository.Add(enity);
+            repository.Remove(enity);
+
+            // Assert
+            var entries = repository.GetAll();
+            entries.Should().HaveCount(0);
+            entries.FirstOrDefault().Should().BeNull();
         }
     }
 }
