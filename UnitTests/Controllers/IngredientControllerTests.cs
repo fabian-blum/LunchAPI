cbc5b4ae4c1714756ae51158d71e6f30c7e21248
Fabian Blum <fa.blum@douglas.de>
2022-05-05 09:04:33 +0200

    Add controller tests

---
+++ UnitTests/Controllers/IngredientControllerTests.cs
@@ -0,0 +1,37 @@
+﻿using LunchAPI.Controllers;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using System;
+using Xunit;
+
+namespace UnitTests.Controllers
+{
+    public class IngredientControllerTests
+    {
+        private ILogger<IngredientController> subLogger;
+
+        public IngredientControllerTests()
+        {
+            this.subLogger = Substitute.For<ILogger<IngredientController>>();
+        }
+
+        private IngredientController CreateIngredientController()
+        {
+            return new IngredientController(
+                this.subLogger);
+        }
+
+        [Fact]
+        public void TestMethod1()
+        {
+            // Arrange
+            var ingredientController = this.CreateIngredientController();
+
+            // Act
+
+
+            // Assert
+            Assert.True(false);
+        }
+    }
+}
---
+++ UnitTests/Controllers/MealControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using LunchAPI.Controllers;
+using LunchAPI.Models;
+using LunchAPI.Repositories.Interfaces;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Xunit;
+
+namespace UnitTests.Controllers
+{
+    public class MealControllerTests
+    {
+        private ILogger<MealController> subLogger;
+        private IRepository<Meal> subRepository;
+
+        public MealControllerTests()
+        {
+            this.subLogger = Substitute.For<ILogger<MealController>>();
+            this.subRepository = Substitute.For<IRepository<Meal>>();
+        }
+
+        private MealController CreateMealController()
+        {
+            return new MealController(
+                this.subLogger,
+                this.subRepository);
+        }
+
+        [Fact]
+        public void GetAll_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var mealController = this.CreateMealController();
+
+            // Act
+            var result = mealController.GetAll();
+
+            // Assert
+            result.Should().BeEmpty();
+        }
+    }
+}
